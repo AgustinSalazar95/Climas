08f65990c3289eccaaeecaae9da7b7af07aa3fc3
AgusSalazar <agustin.salazar@openix.com.ar>
2022-08-03 15:28:23 -0300

    Finalizada

---
+++ src/components/WeatherApp.js
@@ -0,0 +1,45 @@
+import {useState, useEffect} from 'react';
+import WeatherForm from './WeatherForm';
+import WeatherMainInfo from './WeatherMainInfo';
+
+const WeatherApp = () => {
+
+    const [weather,setWeather] = useState(null);
+
+    useEffect(() =>{
+        loadInfo();
+    }, []);
+
+    useEffect(() =>{
+        document.title = `Weather | ${weather?.location.name ?? ""}`;
+    }, [weather]);
+
+    async function loadInfo(city = "london"){
+        try {
+            const request = await fetch(
+                `${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}&q=${city}`
+            );
+            const json = await request.json();
+
+            setWeather(json);
+            console.log(json);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    function handleChangeCity(city){
+        setWeather(null);
+
+        loadInfo(city);
+    }
+
+    return (
+        <div>
+            <WeatherForm onChangeCity={handleChangeCity}/>
+            <WeatherMainInfo  weather={weather}/>
+        </div>
+    )
+}
+
+export default WeatherApp;
